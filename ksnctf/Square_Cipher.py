d334b6b86cc9c84a0157a761b1e1c904662b9877
RyutaKremer <r_kremer@keio.jp>
2020-04-25 00:08:11 +0900

    Add: Square Cipher

---
+++ ksnctf/Square_Cipher.py
@@ -0,0 +1,39 @@
+sc = '''oomktvziqtaovmmpxzoqrzsxlpwpgoj
+uDQEMISYnnVYnvyWRhHsDXnSCXAVVZj
+tZbknedErdpvAwQWpUiLqOxIqpafvXp
+dXoAVWcKppbEPuaqmXWjXJwRoRFOoEg
+pDiRUXlQjKJlslskVpGwtljGyVJPxHv
+bQsQNKxCsdYMdQPJiBmyrsuOrJQOtXg
+pMekeinUaMoDXqFzweLKipkBuggnsUv
+eQFYCJSKfBgHaJgZnZoWmOmAOJLVQHi
+hljrplajyKNXtwmfOjRwOqcqeeplyzy
+gkFOltsOyrPgIaerIaSjQQaVMyEhfyd
+vEaRHbBzfrcwJbCZmHdddLpuEJwspbt
+sXQGkwpKaTZmWJiZzpbkpHNiToawxKn
+wJpIKbGhnLjVAJNcxrqkKEJCKCOocSv
+mTRDNDpFtRUmcHoRELeSqXoGUIIsuYu
+ajeHaSVlQGLaEprSQarDzTomJdAWfqb
+zIJLHRBXMvNDegYeaoVRDuWBbdSBtLv
+xIeKdAYwajGHMgRLDGgDinBiLNBgatb
+kHepNsCQSJjTRmQrCHYWJqIPOVAUOer
+rvhmZfmogPglGNuLyAuSivBctlvVfzb
+qBJdHUkSaTArlgkhtHPyGhXOPkwmkBq
+rvbzZfwvLtTnhyXVHPlwsuGZQnNiNcm
+yCMtAVwYVgtZHVNznolGMBETIHFmoWj
+wfezbysbvOzsAhxSZFFAfOouyHldEYh
+gNHKKSFUtcUxfRyXHMugYBtAxBwDJZh
+rHmsozuNeoJqyzMDHsNbUDwzaNLtdxr
+bVmQMHyNndOWCZLnhrPxZXCYLDTWQre
+aSiEEJjZtoRpUzgsxsiiGzvnRpKLMrk
+qTzGCKvNhUhjrmCjAdwQAvkgqHyJZLm
+sSxzwjxAnWesTszIxirRwcWIXUPtwwa
+nTDEMTRGyhzdCtkTTDWbxdSjsNYlfXz
+eawtidzosgaofjxxyfcdoiulemirqap'''
+
+for c in sc:
+    if (c.isupper()):
+        print("■", end="")
+    elif (c.islower()):
+        print(" ", end="")
+    else:
+        print()
